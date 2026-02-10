52f3a1dd3d29d8e686dfc5a70a3bca5ef9fbd646
quratulain345 <quratulainkhan464@gmail.com>
2026-02-10 13:16:53 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,16 @@
 # this is my new learning python file which i created in my repository
 # go to files and click add file then enter  the name of file for python file add .py 
 #then commit the cahnges to save it
-print("this is my fiest gitHub python file")
+print("this is my first gitHub python file")
+# Program to print the table of 2
+
+def print_table(number):
+    print("Multiplication Table of", number)
+    print("---------------------------")
+    for i in range(1, 11):
+        print(f"{number} x {i} = {number * i}")
+
+# Main program
+if __name__ == "__main__":
+    num = 2
+    print_table(num)
