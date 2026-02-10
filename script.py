0b5f99ad56e466a84a2c6821a15d97e621bb03a9
quratulain345 <quratulainkhan464@gmail.com>
2026-02-10 13:07:30 +0500

    i have updated this file

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
 # this is my new learning python file which i created in my repository
-# go to files and click add file then enter the name of file for python file add .py 
+# go to files and click add file then enter  the name of file for python file add .py 
 #then commit the cahnges to save it
 print("this is my fiest gitHub python file")
